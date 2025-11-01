b3e6a81a01f315e0ed7ccd2b100fc2601616d04e
Devninja <jiaseng.Lam@everleagues.com>
2025-11-01 21:27:27 +0800

    update project path naming and added script to run individual project

--- packages/services/notes-service/src/main.ts
+++ packages/services/notes-service/src/main.ts
@@ -1,21 +1,18 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import express from 'express';
+import express, { Request, Response } from 'express';
 import * as path from 'path';
+import { MessageResponse } from '@note-sharing/shared';
 
 const app = express();
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to notes-service!' });
+app.get('/api', (req: Request, res: Response<MessageResponse>) => {
+  res.send({ message: 'Welcome to notes-service!!' });
 });
 
-const port = process.env.PORT || 3333;
+const port: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
+
 server.on('error', console.error);
