39bbf6cde510477f0348469db3e78d5fc5481318
Devninja <jiaseng.Lam@everleagues.com>
2025-11-01 21:00:41 +0800

    update frontend file and fodler structure

--- packages/frontend/src/app/app.ts
+++ packages/frontend/src/app/app.ts
@@ -9,5 +9,5 @@ import { NxWelcome } from './nx-welcome';
   styleUrl: './app.scss',
 })
 export class App {
-  protected title = 'org';
+  protected title = 'my-angular-app';
 }
--- packages/frontend/src/app/nx-welcome.ts
+++ packages/frontend/src/app/nx-welcome.ts
@@ -432,7 +432,7 @@ import { CommonModule } from '@angular/common';
         <div id="welcome">
           <h1>
             <span> Hello there, </span>
-            Welcome org 👋
+            Welcome my-angular-app 👋
           </h1>
         </div>
         <!--  HERO  -->
@@ -801,7 +801,7 @@ nx run-many -t build test lint</pre>
               </svg>
               View project details
             </summary>
-            <pre>nx show project org</pre>
+            <pre>nx show project my-angular-app</pre>
           </details>
 
           <details>
